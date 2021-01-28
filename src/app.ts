1c6b0e634753270af813ef355ecb210436c5e995
leynier <leynier41@gmail.com>
2021-01-28 02:46:27 -0500

    Insert test endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
-import { Server } from "@hapi/hapi"
+import { Request, ResponseToolkit, Server } from "@hapi/hapi"
 import { syncController } from './sync.controller'
 
 export const init = async () => {
     const server: Server = new Server({
         host: process.env.HOST || 'localhost',
         port: process.env.PORT || 4000,
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/test',
+        handler: (req: Request, h: ResponseToolkit) => {
+            return h.response('Covid19 Cuba API')
+        }
     })
 
     server.route({
